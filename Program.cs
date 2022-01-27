caec18aad8f6d73835a09a08193627e94139f6bf
ryannas <ryannas@163.com>
2022-01-27 22:48:05 +0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VisualStudio2022_CSharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
